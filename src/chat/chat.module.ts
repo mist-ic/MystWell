0cfbdf21fe9af7dbbe22636002f71723b622b092
mist-ic <mysticscholarate@gmail.com>
2025-05-18 03:39:21 +0530

    fix: minor bug fixes

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -5,12 +5,14 @@ import { ConfigModule } from '@nestjs/config';
 import { SupabaseModule } from '../supabase/supabase.module';
 import { ChatGateway } from './chat.gateway';
 import { DocumentModule } from '../document/document.module';
+import { TranscriptionModule } from '../transcription/transcription.module';
 
 @Module({
   imports: [
     ConfigModule,
     SupabaseModule,
     DocumentModule,
+    TranscriptionModule,
   ],
   controllers: [ChatController],
   providers: [ChatService, ChatGateway],
